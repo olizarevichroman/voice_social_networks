14b4a3fb79ec467f1adc6439e0ccf3cb6bc8a5f7
Raman Alizarevich <Raman_Alizarevich@epam.com>
2020-02-08 17:39:57 +0300

    Commented UsersController to avoid exceptions

--- VoiceSocialNetworks/VoiceSocialNetworks/Controllers/UsersController.cs
+++ VoiceSocialNetworks/VoiceSocialNetworks/Controllers/UsersController.cs
@@ -1,32 +1,32 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using VoiceSocialNetworks.DataLayer.Implementations;
-using VoiceSocialNetworks.DataLayer.Models;
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using VoiceSocialNetworks.DataLayer.Implementations;
+//using VoiceSocialNetworks.DataLayer.Models;
 
-namespace VoiceSocialNetworks.Controllers
-{
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class UsersController : ControllerBase
-    {
-        private readonly UserRepository _repository;
-        public UsersController(UserRepository repository)
-        {
-            _repository = repository;
-        }
-        [HttpGet]
-        public IActionResult GetUser()
-        {
-            var user = HttpContext.User;
-            var identity = user.Identity;
+//namespace VoiceSocialNetworks.Controllers
+//{
+//    [Route("api/[controller]/[action]")]
+//    [ApiController]
+//    public class UsersController : ControllerBase
+//    {
+//        private readonly UserRepository _repository;
+//        public UsersController(UserRepository repository)
+//        {
+//            _repository = repository;
+//        }
+//        [HttpGet]
+//        public IActionResult GetUser()
+//        {
+//            var user = HttpContext.User;
+//            var identity = user.Identity;
 
-            var _ = new
-            {
-                identity.Name,
-                identity.IsAuthenticated
-            };
+//            var _ = new
+//            {
+//                identity.Name,
+//                identity.IsAuthenticated
+//            };
 
-            return new JsonResult(_);
-        }
-    }
-}
+//            return new JsonResult(_);
+//        }
+//    }
+//}
