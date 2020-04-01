79c86a2f4bf9600596db0370d46549f40353bec9
Raman Alizarevich <Raman_Alizarevich@epam.com>
2020-04-01 23:25:07 +0300

    Returned back changes

--- VoiceSocialNetworks/VoiceSocialNetworks/Controllers/AliceController.cs
+++ VoiceSocialNetworks/VoiceSocialNetworks/Controllers/AliceController.cs
@@ -41,19 +41,19 @@ namespace VoiceSocialNetworks.Controllers
                     return Ok(result);
                 }
             }
-            //var user = HttpContext.User;
-            //if (!user.Identity.IsAuthenticated)
-            //{
-            //    result.StartAccountLinking = new object();
+            var user = HttpContext.User;
+            if (!user.Identity.IsAuthenticated)
+            {
+                result.StartAccountLinking = new object();
 
-            //    return await Task.FromResult(Ok(result));
-            //}
-            //var userId = user.FindFirst("id").Value;
-            //var yandexUser = await _unitOfWork.UserRepository.GetEntity(userId);
-            //if (yandexUser == null)
-            //{
-            //    await _userCreator.SyncYandexUser(user.Identity as ClaimsIdentity);
-            //}
+                return await Task.FromResult(Ok(result));
+            }
+            var userId = user.FindFirst("id").Value;
+            var yandexUser = await _unitOfWork.UserRepository.GetEntity(userId);
+            if (yandexUser == null)
+            {
+                await _userCreator.SyncYandexUser(user.Identity as ClaimsIdentity);
+            }
 
             result.Response = new Response
             {
